251ac2c7535dc6f5a06833c59a184a67726cb91c
Kevin Silas <Kwsilas@gmail.com>
2015-10-03 20:47:36 -0400

    add signup loginin to models and index route

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
 var Like = require('./like');
 
 var UserSchema = new mongoose.Schema({
@@ -12,5 +13,13 @@ var UserSchema = new mongoose.Schema({
   location: String,
   saved_events: [{ type: mongoose.Schema.ObjectId, ref: 'Event' }]
 });
+
+UserSchema.methods.encrypt = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8));
+};
+
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.local.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
