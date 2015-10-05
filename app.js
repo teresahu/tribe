b68c04f80274205214ff65bcf2e18b876f59cf10
Kevin Silas <Kwsilas@gmail.com>
2015-10-05 16:04:17 -0400

    correct routes and signup login

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.use('/', routes);
-app.use('/users', users);
+// app.use('/', routes);
+app.use('/', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var passport = require('passport');
+
 var router = express.Router();
 
 /* GET home page. */
@@ -7,42 +7,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tribe' });
 });
 
-// GET /signup
-router.get('/signup', function(req, res, next) {
-  res.render('signup.ejs', { message: req.flash() });
-});
-
-// POST /signup
-router.post('/signup', function(req, res, next) {
-  var signUpStrategy = passport.authenticate('local-signup', {
-    successRedirect : '/views', // double check route???
-    failureRedirect : '/signup',
-    failureFlash : true
-  });
-
-  return signUpStrategy(req, res, next)
-});
-
-// GET /login
-router.get('/login', function(req, res, next) {
-  res.render('login.ejs', { message: req.flash() });
-});
-
-// POST /login
-router.post('/login', function(req, res, next) {
-  var loginProperty = passport.authenticate('local-login', {
-    successRedirect : '/views',
-    failureRedirect : '/login',
-    failureFlash : true
-  });
-
-  return loginProperty(req, res, next);
-});
-
-// GET /logout
-router.get('/logout', function(req, res, next) {
-  req.logout();
-  res.redirect('/');
-});
 
 module.exports = router;
